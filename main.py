a84b320abb987a4222f2981bb14f0a87daabecd4
LuisLeon-Dev <lleon5073@gmail.com>
2024-04-11 09:15:30 -0600

    creating the paddles and adding the respective moves

--- main.py
+++ main.py
@@ -1,32 +1,31 @@
-from turtle import Screen, Turtle
+from turtle import Screen
+
+from paddle import Paddle
 
 screen = Screen()
-paddle = Turtle()
+r_paddle = Paddle(x=350)
+l_paddle = Paddle(x=-350)
 
 # Setting the screen
 screen.setup(width=800, height=600)
 screen.bgcolor('black')
 screen.title("Pong Game")
-
-paddle.shape("square")
-paddle.color("white")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.penup()
-paddle.setpos(x=350, y=0)
+screen.tracer(0)
 
 
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
+# right paddle 
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, key="Down")
 
-
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
-
-
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, key="Down")
+# left paddle
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, key="s")
 screen.listen()
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+
 
 screen.exitonclick()
