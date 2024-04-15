02e1a58418997ccf986a63c72a18bab0aa8d40f1
LuisLeon-Dev <lleon5073@gmail.com>
2024-04-15 13:17:33 -0600

    collison with paddle logic

--- ball.py
+++ ball.py
@@ -16,6 +16,8 @@ class Ball(Turtle):
         y_cor = self.ycor() + self.y_move
         self.goto(x_cor, y_cor)
 
-    def bounce(self):
+    def bounce_y(self):
         self.y_move *= -1
 
+    def bounce_x(self):
+        self.x_move *= -1
