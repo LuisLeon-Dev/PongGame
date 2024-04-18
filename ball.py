6f51e61e13959fe64a35dbb8413cd5bd818b2e9f
LuisLeon-Dev <lleon5073@gmail.com>
2024-04-17 23:15:28 -0600

    finishing the game adding the score board logic

--- ball.py
+++ ball.py
@@ -8,8 +8,9 @@ class Ball(Turtle):
         self.shape('circle')
         self.color("white")
         self.penup()
-        self.x_move = 2
-        self.y_move = 2
+        self.x_move = 10
+        self.y_move = 10
+        self.move_speed = 0.1
 
     def move(self):
         x_cor = self.xcor() + self.x_move
@@ -21,3 +22,9 @@ class Ball(Turtle):
 
     def bounce_x(self):
         self.x_move *= -1
+        self.move_speed *= 0.9
+
+    def reset_position(self):
+        self.goto(0, 0)
+        self.move_speed = 0.1
+        self.bounce_x()
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from turtle import Screen
 import time
 from paddle import Paddle
 from ball import Ball
+from score_board import Scoreboard
 
 
 screen = Screen()
@@ -16,6 +17,7 @@ screen.title("Pong Game")
 screen.tracer(0)
 
 ball = Ball()
+score_board = Scoreboard()
 
 # right paddle
 screen.onkey(r_paddle.go_up, "Up")
@@ -30,7 +32,7 @@ screen.listen()
 
 game_is_on = True
 while game_is_on:
-    time.sleep(.01)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
@@ -42,5 +44,14 @@ while game_is_on:
     if ball.distance(r_paddle) < 50 and ball.xcor() > 330 or ball.distance(l_paddle) < 50 and ball.xcor() > -330:
         ball.bounce_x()
 
+    # Detect R paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        score_board.l_point()
+
+    # Detect L paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+        score_board.r_point()
 
 screen.exitonclick()
